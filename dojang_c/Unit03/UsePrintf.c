6f955687cfd31f8be916a23bef817b3724a14d90
Dialogicode <104162300+dialogicode@users.noreply.github.com>
2022-05-11 03:03:40 +0900

    C/C++ vscode 컴파일 환경 설정 및 printf 써보기

---
+++ dojang_c/Unit03/UsePrintf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	// 서식 지정자 사용하기
+	// 변수를 사용하여 같은 내용을 여러번 출력하거나, 출력형태를 바꿀때 유용
+
+	// %s 부분에 문자열 넣기
+	printf("%s\n", "Hello World!");
+	printf("%s %s\n", "Hello", "Developer");
+
+	return 0;
+}
